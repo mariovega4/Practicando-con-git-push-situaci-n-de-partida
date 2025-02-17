a7722b98de0b5262f0d9e73e1e4a060e5f3fbfc6
clear <mariovegasantos3@gmail.com>
2025-02-17 14:03:16 +0100

    App.java y Readme.md modificados

--- App.java
+++ App.java
@@ -1,9 +1,13 @@
 public class App {
   
   public static void main(String[] args) {
+
+    System.out.println("Mario Vega Santos");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("IES San Andrés");
+    System.out.println("León");
   }
   
 }
